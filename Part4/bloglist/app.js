d2bde74b4f0a8c8485be7a3336aa299d9e6eea9a
Gaganish Yadav <87760104+gaganishyadav@users.noreply.github.com>
2025-02-11 19:17:15 +0530

    Updated app

---
+++ Part4/bloglist/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs',blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
